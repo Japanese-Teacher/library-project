7d81cd979a746ae969bf00b722a3b45cab7b4991
Lavochcka <leva.kostetskiy13@mail.ru>
2025-03-19 18:05:23 +0300

    feat: created menu

--- app/main.py
+++ app/main.py
@@ -1,2 +1,27 @@
+def main() -> None:
+    open_menu()
+    command = input('\nВведите команду: ')
+    process_command(command)
+    while command != '0':
+        if command != '1':
+            print('\nВы ввели отсутствующую команду')
+        print('\nПопробуйте снова')
+        open_menu()
+        command = input('\nВведите команду: ')
+        process_command(command)
+
+
+def open_menu() -> None:
+    print('\n1. Добавить книгу')
+    print('0. Завершить работу')
+
+
+def process_command(command: str) -> None:
+    if command == '0':
+        print('\nРабота завершена')
+    elif command == '1':
+        print('\nКоманда не реализована')
+
+
 if __name__ == '__main__':
-    print('Hello world')
+    main()
