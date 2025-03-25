7abffef6a145c9ff41a043f1ebb5e0af87335424
Lavochcka <leva.kostetskiy13@mail.ru>
2025-03-25 14:57:20 +0300

    feat: added third command to utils in main and process_command

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,8 @@
 from app.interface.add_book_menu import add_book_menu
+from app.interface.edit_book_menu import edit_book_menu, choose_book_to_edit_menu
 from app.interface.open_menu import open_menu
 from app.services.add_book import add_book
+from app.services.edit_book import choose_book_to_edit
 from app.services.print_books import print_books
 
 
@@ -9,7 +11,7 @@ def main() -> None:
     command = input('\nВведите команду: ')
     process_command(command)
     while command != '0':
-        if command != '1' and command != '2':
+        if command != '1' and command != '2' and command != '3':
             print('\nВы ввели отсутствующую команду')
             print('\nПопробуйте снова')
         open_menu()
@@ -25,3 +27,8 @@ def process_command(command: str) -> None:
         add_book()
     elif command == '2':
         print_books()
+    elif command == '3':
+        choose_book_to_edit_menu()
+        choose_book_to_edit()
+        edit_book_menu()
+
