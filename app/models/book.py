2f47d2fb0b517720fdb05b0a16f80250664b9363
Lavochcka <leva.kostetskiy13@mail.ru>
2025-03-25 03:21:48 +0300

    feat: added index to model Book

--- app/models/book.py
+++ app/models/book.py
@@ -1,9 +1,11 @@
 class Book:
 
-    def __init__(self, name: str, author: str):
+    def __init__(self, index: int, name: str, author: str):
+        self.index = index
         self.name = name
         self.author = author
 
     def __str__(self) -> str:
-        return f'''Имя книги: {self.name}
+        return f'''Номер книги: {self.index}
+Имя книги: {self.name}
 Автор: {self.author}'''
