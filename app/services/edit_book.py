d48294b9c8fae6d12a565df133f6861c528e59d1
Lavochcka <leva.kostetskiy13@mail.ru>
2025-03-25 16:08:44 +0300

    feat: added attribute to argument index, comments to input and \n to prints with

--- app/services/edit_book.py
+++ app/services/edit_book.py
@@ -4,16 +4,16 @@ from app.services.add_book import book_list
 def choose_book_to_edit() -> int:
     for book in book_list:
         print(book.index, book.name, book.author, sep=" ")
-    index = int(input()) - 1
+    index = int(input('Введите номер книги: ')) - 1
     return index
 
-def edit_book(index) -> None:
-    command = input()
+def edit_book(index: int) -> None:
+    command = input('Введите номер параметра книги: ')
     if command == '1':
-        print('Введите новое название: ')
+        print('\nВведите новое название: ')
         new_name = input()
         book_list[index].name = new_name
     elif command == '2':
-        print('Введите нового автора: ')
+        print('\nВведите нового автора: ')
         new_author = input()
         book_list[index].author = new_author
